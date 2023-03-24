a9752fbe3455cfa2b8835b5b42b02bef8e9edc3e
GPFBKK\triphop.pro <triphop.pro@gmail.com>
2023-03-24 15:28:20 +0700

    Movies-Reccommend

---
+++ src/top100Films.js
@@ -0,0 +1,7 @@
+const top100Films = [
+    { label: 'The Shawshank Redemption', year: 1994 },
+    { label: 'The Godfather', year: 1972 },
+    { label: 'The Godfather: Part II', year: 1974 },
+  ];
+
+  export default top100Films
